1f1ada62578c32e48b5823661388ed8b2a544879
nabeel7736 <mr.nabeelmp@gmail.com>
2025-10-14 09:57:21 +0530

    Time: 36 ms (10.69%), Space: 11.8 MB (43.22%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -0,0 +1,12 @@
+func containsDuplicate(nums []int) bool {
+    seen := make(map[int]bool)
+
+    for _, num := range nums{
+     if _,found :=seen[num]; found {
+        return true
+     }   
+
+     seen[num] = true
+    }
+    return false
+}
