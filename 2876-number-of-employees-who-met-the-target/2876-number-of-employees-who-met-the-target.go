c72aa0712e955a8c041f9d60a0d8ad9ee1487aaf
nabeel7736 <mr.nabeelmp@gmail.com>
2025-10-18 10:35:13 +0530

    Time: 0 ms (100%), Space: 4.5 MB (83.33%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.go
@@ -0,0 +1,10 @@
+func numberOfEmployeesWhoMetTarget(hours []int, target int) int {
+    count := 0
+
+    for _, hour := range hours {
+        if hour >= target {
+            count++
+        }
+    }
+        return count
+}
